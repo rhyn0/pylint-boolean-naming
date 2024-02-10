dc5e35fdb2fab0a12ed0debc12bc61bd80faff31
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-02-10 12:00:12 -0800

    add: output update class for testing

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -2,10 +2,14 @@
 from pathlib import Path
 import sys
 from typing import List
+from typing import Union
 
 # External Party
+from pylint.testutils import UPDATE_FILE
+from pylint.testutils import UPDATE_OPTION
 from pylint.testutils import FunctionalTestFile
 from pylint.testutils import LintModuleTest
+from pylint.testutils.functional import LintModuleOutputUpdate
 import pytest
 
 TEST_DIR = Path(__file__).parent
@@ -19,6 +23,16 @@ sys.path += [
 # Credit to pylint-django
 # https://github.com/pylint-dev/pylint-django
 class PylintBooleanModuleTest(LintModuleTest):
+    """Only used so that we can load this checker into the linter!"""
+
+    def __init__(self, test_file: FunctionalTestFile):
+        """Force loading of our checker."""
+        super().__init__(test_file)
+        self._linter.load_plugin_modules(["pylint_boolean_naming"])
+        self._linter.load_plugin_configuration()
+
+
+class PylintBooleanOutputUpdate(LintModuleOutputUpdate):
     """Only used so that we can load this checker into the linter!"""
 
     def __init__(self, test_file: FunctionalTestFile):
@@ -58,7 +72,28 @@ TESTS_NAMES = [t.base for t in TESTS]
 
 @pytest.mark.parametrize("test_file", TESTS, ids=TESTS_NAMES)
 def test_everything(test_file: FunctionalTestFile):
+    """Run tests collected from the input directory.
+
+    Uses the above defined classes to force the loading of our checker.
+    """
     # copied from pylint.tests.test_functional.test_functional
-    lint_test = PylintBooleanModuleTest(test_file)
+    lint_test: Union[PylintBooleanModuleTest, PylintBooleanOutputUpdate]
+    lint_test = (
+        PylintBooleanOutputUpdate(test_file)
+        if UPDATE_FILE.exists()
+        else PylintBooleanModuleTest(test_file)
+    )
     lint_test.setUp()
     lint_test.runTest()
+
+
+if __name__ == "__main__":
+    # Helper function to update the expected output of the tests
+    if UPDATE_OPTION in sys.argv:
+        UPDATE_FILE.touch()
+        sys.argv.remove(UPDATE_OPTION)
+    try:
+        pytest.main(sys.argv)
+    finally:
+        if UPDATE_FILE.exists():
+            UPDATE_FILE.unlink()
